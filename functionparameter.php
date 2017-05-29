8c162b1e8280a29112022b0eb2ed5964695d933d
Ohisthie <isthie.olivia@gmail.com>
2017-05-29 14:31:43 +0700

    Add files via upload

---
+++ functionparameter.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+<title>Function dengan Paramter</title>
+</head>
+<body>
+<?php
+echo"<b>";
+	function panggil($parameter)
+	{
+		echo"Saya Bilang : $parameter";
+		echo"<br>";
+	}
+	panggil ("Belajar PHP rupanya <u>MUDAH</u>");
+	panggil ("Asyik...");
+	panggil ("Dan Sangat Menyenangkan");
+	panggil ("Nilai saya php adalah 98");
+	echo"</b>";
+?>
+</body>
+</html>
